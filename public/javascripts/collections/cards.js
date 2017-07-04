12e67b770d2f7dd2bef5ca1f23b9a4f78557addb
Jay Shenk <jay.shenk@gmail.com>
2017-07-04 16:24:27 -0400

    Refactor

--- public/javascripts/collections/cards.js
+++ public/javascripts/collections/cards.js
@@ -1,5 +1,25 @@
 var Cards = Backbone.Collection.extend({
   model: Card,
   url: '/cards',
-  comparator: 'position'
+  comparator: 'position',
+  decrementPositions: function(removedModel) {
+    this.filter(function(model) {
+      return model.get('position') > removedModel.get('position');
+    }).forEach(function(model) {
+      model.set('position', model.get('position') - 1);
+      model.trigger('moved');
+    });
+  },
+  incrementPositions: function(addedModel) {
+    this.filter(function(model) {
+      return model.get('position') >= addedModel.get('position');
+    }).forEach(function(model) {
+      model.set('position', model.get('position') + 1);
+      model.trigger('moved');
+    });
+  },
+  initialize: function() {
+    this.on('add', this.incrementPositions);
+    this.on('remove', this.decrementPositions);
+  }
 });
