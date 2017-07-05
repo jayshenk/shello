5fcd6201072500a0bb28f04357e73ca171024515
Jay Shenk <jay.shenk@gmail.com>
2017-07-05 10:27:27 -0400

    Adjust labels backend

--- routes/index.js
+++ routes/index.js
@@ -1,12 +1,14 @@
 var path = require("path");
 var Cards = require(path.resolve(path.dirname(__dirname), "modules/cards"));
 var Lists = require(path.resolve(path.dirname(__dirname), "modules/lists"));
+var Labels = require(path.resolve(path.dirname(__dirname), "modules/labels"));
 
 module.exports = function(router) {
   router.get('/', function(req, res, next) {
     res.render('index', {
       cards: Cards.getInitialData(),
-      lists: Lists.get()
+      lists: Lists.get(),
+      labels: Labels.get()
     });
   });
 };
